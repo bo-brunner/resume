0ebe7df7deb2c9320b6b336cb01fd34d46289b54
bo-brunner <164268796+bo-brunner@users.noreply.github.com>
2024-03-24 14:30:26 -0500

    Create js.js

---
+++ js.js
@@ -0,0 +1,74 @@
+
+
+function showLocation1(){
+  var x = document.getElementById("location1")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showLocation2(){
+  var x = document.getElementById("location2")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showLocation3(){
+  var x = document.getElementById("location3")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showLocation4(){
+  var x = document.getElementById("location4")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showLocation5(){
+  var x = document.getElementById("location5")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showLocation6(){
+  var x = document.getElementById("location6")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showLocation7(){
+  var x = document.getElementById("location7")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showLocation8(){
+  var x = document.getElementById("location8")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
